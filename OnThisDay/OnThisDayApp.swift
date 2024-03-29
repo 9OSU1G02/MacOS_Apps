08479a144c184b8e81cd9c3b05363ce7b4b3f68b
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-03-29 10:01:41 +0700

    Initial Commit

---
+++ OnThisDay/OnThisDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnThisDayApp.swift
+//  OnThisDay
+//
+//  Created by Quốc Huy Nguyễn on 29/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OnThisDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
